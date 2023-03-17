f52336b435dfbf8b5c011d6321ec784ba747296d
FaridRifaan <faridrifan59@gmail.com>
2023-03-18 00:47:50 +0700

    Latihan Cloning Shopee

---
+++ app/src/main/java/com/project/latihan_shopee_farid/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.project.latihan_shopee_farid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
